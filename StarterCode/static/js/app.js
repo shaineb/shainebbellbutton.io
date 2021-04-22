40ecc64953ed6c021abaa00bab670b66062c07f7
infin <infin@DESKTOP-CGOTLLG>
2021-04-21 21:36:06 -0500

    more data added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,16 @@
-// create bar chart
+//  function to create bar chart and bubble chart
+function bubblebarbuild( sample){ 
+    d3.json ("samples.json").then((data) => {
+        var bellybutton_id = data.samples;
+        var filter_id_array = bellybutton_id.filter(sampleobject=>sampleobject.id == sample)
+        var  filter_id_result = filter_id_array[0];
+        var  otu_ids = filter_id_result.otu_ids;
+        var  otu_labels = filter_id_result.otu_labels;
+        var sample_values = filter_id_result.sample_values; 
+    
 var bar_data = [{
     x:sample_values.slice(0,10).reverse (),
-    y:ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+    y:otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
     type:"bar",
     orientation:"h"
 
@@ -14,21 +23,27 @@ var barlayout = {
     margin:{t:30,l:150}
     
 };
+
+Plotly.newPlot("bar",bar_data,barlayout);
 //build bubble chart
 
-var layoutbubble ={
-    margin:{t,0},
-    xaxis:{ title:"id"},
+var layoutbubble = {
+    margin:{t:0},
+    xaxis:{ title:"OTU id"},
     hovermode:"closest",
 };
 var DataBubble = [
 {
-    x:id,
+    x:otu_ids,
     y:sample_values,
-    text:"markers",
+    mode:"markers",
+    text:otu_labels,
     marker:{
         color:id,
         size:sample_values
     }
-}
-];
+}];
+Plotly.newPlot("bubble",DataBubble,layoutbubble);
+
+    });
+    }
